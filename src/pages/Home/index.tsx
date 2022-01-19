6fa5e27a2379996cd09bc7237bcfec45fc3c7cf2
tim <timur.kim@rocknblock.io>
2022-01-19 15:35:17 +0300

    testing fonts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,10 @@ import { FC } from 'react';
 import s from './Home.module.scss';
 
 const Home: FC = () => {
-  return <div className={s.home_wrapper}>Hello RnB</div>;
+  return (
+    <div className={s.home_wrapper}>
+      <div className={s.title}>Hello RnB</div>
+    </div>
+  );
 };
 export default Home;

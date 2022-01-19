30d689f6f2cd6df6d10999bf7345d449476e14cd
tim <timur.kim@rocknblock.io>
2022-01-19 17:58:42 +0300

    added new configs for polyfills

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
 
 import s from './Home.module.scss';
+// import Logo from 'assets/img/icons/logo.svg';
 
 const Home: FC = () => {
   return (
     <div className={s.home_wrapper}>
       <div className={s.title}>Hello RnB</div>
+      {/* <Logo /> */}
     </div>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import inject from '@rollup/plugin-inject';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -13,14 +15,31 @@ export default defineConfig({
   ],
   build: {
     rollupOptions: {
-      plugins: [inject({ Buffer: ['buffer', 'Buffer'] })],
+      plugins: [rollupNodePolyFill()],
     },
     commonjsOptions: {
       transformMixedEsModules: true,
     },
   },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
   resolve: {
     alias: {
+      process: 'rollup-plugin-node-polyfills/polyfills/process-es6',
+      Events: 'rollup-plugin-node-polyfills/polyfills/events',
+      Buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
       web3: path.resolve(__dirname, './node_modules/web3/dist/web3.min.js'),
     },
   },

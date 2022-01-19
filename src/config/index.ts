b3b2f375cab4ed10d2b89c8dd8f7fcec67a4f8f3
Gleb Chistyakov <gleb4ist@gmail.com>
2022-01-19 10:45:59 +0300

    fix import styles

--- src/config/index.ts
+++ src/config/index.ts
@@ -36,36 +36,6 @@ export const chains: {
   },
 };
 
-// export const chains: {
-//   [key: string]: {
-//     name: chainsEnum;
-//     network: INetwork;
-//     provider: {
-//       [key: string]: any;
-//     };
-//     explorer: string;
-//   };
-// } = {
-//   [chainsEnum['Binance-Smart-Chain']]: {
-//     name: chainsEnum['Binance-Smart-Chain'],
-//     network: {
-//       chainID: import.meta.env.VITE_CHAIN_ID,
-//       chainName: import.meta.env.VITE_CHAIN_NAME,
-//       nativeCurrency: {
-//         name: 'BNB',
-//         symbol: 'BNB',
-//         decimals: 18,
-//       },
-//       rpc: import.meta.env.VITE_RPC,
-//       blockExplorerUrl: import.meta.env.VITE_BLOCK_EXPLORER_URL,
-//     },
-//     provider: {
-//       MetaMask: { name: 'MetaMask' },
-//     },
-//     explorer: import.meta.env.VITE_EXPLORER,
-//   },
-// };
-
 export const connectWallet = (chainName: chainsEnum): IConnectWallet => {
   const chain = chains[chainName];
 

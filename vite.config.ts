5713bf234fe9b4638ccd9a6bb7d132b1cf352fc9
tim <timur.kim@rocknblock.io>
2022-01-19 14:23:20 +0300

    fix prod build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 // import legacy from '@vitejs/plugin-legacy';
+import inject from '@rollup/plugin-inject';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
@@ -12,6 +13,14 @@ export default defineConfig({
       root: './',
     }),
   ],
+  build: {
+    rollupOptions: {
+      plugins: [inject({ Buffer: ['buffer', 'Buffer'] })],
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
   resolve: {
     alias: {
       web3: path.resolve(__dirname, './node_modules/web3/dist/web3.min.js'),

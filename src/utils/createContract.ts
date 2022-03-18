79c5237d426e01a71444188127349bfd9e881d02
kalinichenko <ka.ki.ko.1995@gmail.com>
2022-03-18 17:46:27 +0300

    fixed template component deps

--- src/utils/createContract.ts
+++ src/utils/createContract.ts
@@ -1,9 +1,10 @@
+import { isMainnet } from 'config/constants';
 import { AbiItem } from 'web3-utils';
 
 import { getWeb3 } from '.';
 
 export const createContract = async (address: string, abi: AbiItem[]) => {
-  const web3 = await getWeb3();
+  const web3 = await getWeb3(isMainnet);
   const contract = new web3.eth.Contract(abi, address);
   return contract;
 };
--- src/utils/getWeb3.ts
+++ src/utils/getWeb3.ts
@@ -1,11 +1,7 @@
 import Web3 from 'web3';
 
-import { isMainnet } from 'config/constants';
-
-export const getWeb3 = async () => {
-  const rpcUrl = isMainnet
-    ? 'https://bsc-dataseed.binance.org/'
-    : 'https://data-seed-prebsc-2-s1.binance.org:8545/';
+export const getWeb3 = async (isMainnet: boolean) => {
+  const rpcUrl = isMainnet ? 'https://bsc-dataseed.binance.org/' : 'https://data-seed-prebsc-2-s1.binance.org:8545/';
   const web3 = new Web3(rpcUrl);
   return web3;
 };

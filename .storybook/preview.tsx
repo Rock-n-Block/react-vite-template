5587730d13bd201f1a30ef272731c0131b7c5eee
kalinichenko <ka.ki.ko.1995@gmail.com>
2022-03-23 14:51:34 +0300

    added vite-sotybook config params, fixed bep20 abi

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-
 import { addDecorator } from '@storybook/react';
 
 const MainDecorator = (story) => {
--- src/config/abi/index.ts
+++ src/config/abi/index.ts
@@ -1,3 +1,2 @@
-export { default as bep20Abi } from './bep20';
 export { default as erc20Abi } from './erc20';
 export { default as stakingAbi } from './staking';

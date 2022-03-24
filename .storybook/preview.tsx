304d735ff0441b23d792ea8e29e8eae75e7301f2
kalinichenko <ka.ki.ko.1995@gmail.com>
2022-03-24 14:49:22 +0300

    added centered position for storybook cmps

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { addDecorator } from '@storybook/react';
 
 const MainDecorator = (story) => {
-  return <Router>{story()}</Router>;
+  const styles: CSSProperties = {
+    width: '100%',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  };
+
+  return (
+    <div style={styles}>
+      <Router>{story()}</Router>
+    </div>
+  );
 };
 
 addDecorator(MainDecorator);

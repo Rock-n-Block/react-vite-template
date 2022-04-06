799cdef8a0fa3d821da2f8a8ba51600ca675f6b9
Vyacheslav Ryutin <ryutin.vyacheslav@gmail.com>
2022-04-06 14:12:14 +0800

    enable strict mode for ts & fix errors

--- src/services/WalletConnect/index.tsx
+++ src/services/WalletConnect/index.tsx
@@ -65,7 +65,7 @@ const WalletConnectContext: FC = ({ children }) => {
 
           setCurrentSubsciber(sub);
         } catch (error: any) {
-          console.log(error);
+          console.error(error);
           // metamask doesn't installed,
           // redirect to download MM or open MM on mobile
           if (error.code === 4) {

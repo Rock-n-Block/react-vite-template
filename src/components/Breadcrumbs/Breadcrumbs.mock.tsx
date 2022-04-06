1e0f5a74e538c810675fb01e032a3987b40cec0c
Denis <aimazingfeed@gmail.com>
2022-04-06 17:00:52 +0300

    Add: Breadcrumbs component was added

---
+++ src/components/Breadcrumbs/Breadcrumbs.mock.tsx
@@ -0,0 +1,36 @@
+import { BreadcrumbsProps } from './Breadcrumbs';
+
+export const breadcrumbsListMocked: BreadcrumbsProps[] = [
+  {
+    paths: [
+      {
+        label: 'Explore',
+        path: '/',
+      },
+      {
+        label: 'Account',
+        path: '/',
+      },
+      {
+        label: 'User NFt Card',
+        path: '/',
+      },
+    ],
+  },
+  {
+    paths: [
+      {
+        label: '1',
+        path: '/',
+      },
+      {
+        label: '123',
+        path: '/123',
+      },
+    ],
+  },
+];
+
+export const breadcrumbsPropsMocked: BreadcrumbsProps = {
+  paths: [],
+};
---
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,42 @@
+import { VFC } from 'react';
+
+import cn from 'clsx';
+
+import { NavLink } from 'react-router-dom';
+import styles from './styles.module.scss';
+
+export type BreadcrumbsPaths = {
+  path: string;
+  label: string;
+};
+export type BreadcrumbsProps = {
+  paths: BreadcrumbsPaths[];
+  className?: string;
+};
+
+export const Breadcrumbs: VFC<BreadcrumbsProps> = ({ paths }) => {
+  return (
+    <nav className={cn(styles.breadcrumbs)}>
+      <ul className={cn(styles.breadcrumbsContainer)}>
+        {paths.length > 1 &&
+          paths.map(({ label, path }, index) => (
+            <li
+              key={path}
+              className={
+              // eslint-disable-next-line no-nested-ternary
+              index === 0
+                ? cn(styles.breadcrumbFirst)
+                : (index === paths.length - 1 ? cn(styles.breadcrumbLast) : cn(styles.breadcrumb))
+            }
+            >
+              <NavLink
+                to={path}
+              >
+                {label}
+              </NavLink>
+            </li>
+          ))}
+      </ul>
+    </nav>
+  );
+};

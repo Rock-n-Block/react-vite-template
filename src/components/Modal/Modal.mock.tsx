574df452dbc53f5c6493a62a8f1768ec1a03da1c
Aimazing <aimazingfeed@gmail.com>
2022-04-12 20:49:18 +0300

    Component Modal was updated

--- src/components/Modal/Modal.mock.tsx
+++ src/components/Modal/Modal.mock.tsx
@@ -1,7 +1,6 @@
 import { ModalProps } from './Modal';
 
 export const modalPropsMocked: ModalProps = {
-  customTitle: 'title',
-  open: true,
-  onClose: () => {},
+  isOpen: true,
+  onClose: () => '',
 };
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,31 +1,79 @@
-import { FC, ReactNode, useMemo } from 'react';
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import { FC, useEffect } from 'react';
 
-import clsx from 'clsx';
-import { omit } from 'lodash';
-import DialogWrap, { DialogProps } from 'rc-dialog';
-import s from './styles.module.scss';
+import cn from 'clsx';
+import { Card } from 'components/Card';
+import { CloseIcon } from 'assets/icons/icons/components';
+import { Text } from 'components/Typography';
+import styles from './styles.module.scss';
 
-export interface ModalProps extends DialogProps {
-  customTitle?: string | ReactNode;
-  size?: 'sm' | 'md' | 'lg';
-  open: boolean;
-  onClose: () => void;
+type ModalSize = {
+  width?: string;
+  height?: string;
+};
+export interface ModalProps {
   className?: string;
+  isOpen: boolean;
+  onClose: (props: unknown) => void;
+  isBackground?: boolean;
+  isDisabledScroll?: boolean;
+  size?: ModalSize
+  title?: string;
+
 }
 
-export const Modal: FC<ModalProps> = ({ children, customTitle = ' ', ...props }) => {
-  const jsxTitle = useMemo(() => {
-    if (typeof customTitle === 'string') {
-      return <div className={clsx(s.title, 'l')}>{customTitle}</div>;
-    }
-
-    return customTitle;
-  }, [customTitle]);
-
+export const Modal: FC<ModalProps> = ({
+  children,
+  className,
+  isOpen,
+  isBackground = true,
+  isDisabledScroll = true,
+  title,
+  onClose,
+  size,
+}) => {
+  useEffect(() => {
+    const { body } = document;
+    const modalCard = document.getElementsByClassName('modalCard')[0];
+    isDisabledScroll && isOpen ? body.style.setProperty('overflow', 'hidden') : body.style.setProperty('overflow', 'visible');
+    isOpen && size ? modalCard.setAttribute('style', `width:${size.width} !important; height:${size.height} !important`) : '';
+  }, [isDisabledScroll, isOpen, size]);
   return (
-    <DialogWrap {...omit(props, 'customTitle')}>
-      <div className={s.modalTitle}>{customTitle && jsxTitle}</div>
-      {children}
-    </DialogWrap>
+    <>
+      <div
+        className={cn(
+          styles.modalOutside,
+          { [styles.closed]: !isOpen },
+          { [styles.backgroundColor]: isBackground },
+        )}
+        onClick={onClose}
+      />
+      <div
+        className={cn(
+          styles.modal,
+          className,
+          { [styles.closed]: !isOpen },
+        )}
+      >
+        <Card className={cn(styles.modalContent, 'modalCard')}>
+          <div
+            className={styles.modalHeader}
+          >
+            <Text size="l">
+              {title}
+            </Text>
+            <div className={styles.closeButton} onClick={onClose}>
+              <CloseIcon />
+
+            </div>
+          </div>
+
+          <div className={styles.modalBody}>
+            {children}
+          </div>
+        </Card>
+      </div>
+    </>
+
   );
 };
